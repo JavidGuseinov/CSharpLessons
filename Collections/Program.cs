deb31ca92505095b1728b3209607ced164617e26
javidguseinov <jaweedguseinov@mail.ru>
2022-04-25 01:26:16 +0300

    Lesson 4

---
+++ Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Javid", "Ali", "Konstantin", "Sadiq" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            List<string> names2 = new List<string>() { "Javid", "Ali", "Konstantin", "Sadiq" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("Fernando");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+            
+
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Added.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Updated.");
+        }
+
+        public int Collect(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public void Collect2(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
