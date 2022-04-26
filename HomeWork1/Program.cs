58511fbc9fe7bbeb50c755fc376336736f9a49fd
javidguseinov <jaweedguseinov@mail.ru>
2022-04-27 01:36:01 +0300

    HomeWork1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dictionaries = new MyDictionary<string>();
+            dictionaries.Add("Javid");
+            Console.WriteLine(dictionaries.Length);
+            dictionaries.Add("Ali");
+            Console.WriteLine(dictionaries.Length);
+            foreach (var dictionary in dictionaries.Items)
+            {
+                Console.WriteLine(dictionary);
+            }
+        }       
+
+    }
+}
